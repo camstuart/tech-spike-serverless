bd058105627fe4b0cc773342454b5114851c6616
Cam Stuart <cstuart@seek.com.au>
2023-03-29 15:28:19 +1100

    feat: Read dynamo & s3

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,38 @@
-import { SQSEvent, SQSHandler } from 'aws-lambda';
-import { DynamoDB } from 'aws-sdk';
+import {SQSEvent, SQSHandler} from 'aws-lambda';
+import * as AWS from 'aws-sdk';
 
-const dynamoDb = new DynamoDB.DocumentClient();
+const region = "ap-southeast-2"
+const dynamoDB: AWS.DynamoDB = new AWS.DynamoDB({
+    region,
+    endpoint: "http://localhost:4566",
+});
+
+const s3Client = new AWS.S3({
+    region,
+    endpoint: 'http://s3.localhost.localstack.cloud:4566'
+});
 
 export const handler: SQSHandler = async (event: SQSEvent) => {
-  const records = event.Records;
+    const records = event.Records;
+    for (const record of records) {
+        const message = JSON.parse(record.body);
 
-  for (const record of records) {
-    const message = JSON.parse(record.body);
+        console.log('myFunction invoked with SQS event body: ', message);
 
-    console.log('myFunction invoked with event body: ', message);
+        dynamoDB.listTables({}, (err, data) => {
+            if (err) {
+                console.error('unable to list dynamodb tables with error:', err);
+            } else {
+                console.log('dynamodb tables listing:', data);
+            }
+        });
 
-    const id = message.id;
-    const data = { id, message };
-
-    const params = {
-      TableName: 'my-table',
-      Item: data,
-    };
-
-    try {
-      await dynamoDb.put(params).promise();
-      console.log(`Successfully added item ${id} to DynamoDB`);
-    } catch (err) {
-      console.error(`Error adding item ${id} to DynamoDB: ${err}`);
+        s3Client.listObjectsV2({Bucket: 'my-bucket'}, (err, data) => {
+            if (err) {
+                console.error('unable to list s3 objects with error:', err);
+            } else {
+                console.log('s3 objects listing:', data);
+            }
+        });
     }
-  }
 };
